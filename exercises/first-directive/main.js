ff2d9e2bf8a2aacb0084c8ccedefffa36bc42328
Bruce Bolt <bbolt@ebi.ac.uk>
2015-01-16 12:14:56 +0000

    first directive exercise

--- exercises/first-directive/main.js
+++ exercises/first-directive/main.js
@@ -2,6 +2,10 @@ var exercise = angular.module("exercise",[]);
 
 exercise.directive("flash",function() {
   return function(scope,el,attrs) {
-    // YOUR CODE
+     // YOUR CODE
+     el = el[0];
+     el.on('click', function() {
+       el.remove();
+     });
   }
 })
