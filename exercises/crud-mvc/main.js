0c3e19863556ac3136c9eb045a1f6a43117711c4
Bruce Bolt <bbolt@ebi.ac.uk>
2015-01-16 10:56:11 +0000

    crud mvc exercise

--- exercises/crud-mvc/main.js
+++ exercises/crud-mvc/main.js
@@ -2,10 +2,18 @@ var app = angular.module("exercise",[]);
 
 app.controller("orderList",function($scope) {
   // TODO implement deleting done orders
+  $scope.remove = function(order) {
+    var index = $scope.orders.indexOf(order);
+    $scope.orders.splice(index, 1);
+  }
 });
 
 app.controller("addOrder",function($scope) {
   // TODO implement new orders
+  $scope.createOrder = function() {
+    $scope.orders.push($scope.order);
+    $scope.order = {};
+  }
 });
 
 app.controller("orders",function($scope) {
